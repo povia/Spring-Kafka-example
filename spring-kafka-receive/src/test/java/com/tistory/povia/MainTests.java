676e6e9ff61e084265843eeaff17d6656b9871d0
povia <tjddn020@naver.com>
2020-12-21 12:54:52 +0900

    updating modules

---
+++ spring-kafka-receive/src/test/java/com/tistory/povia/MainTests.java
@@ -0,0 +1,13 @@
+package com.tistory.povia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
